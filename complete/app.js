2819906872c9a861cde4090af8c8f581d9a3bf77
devanshi-j <89416589+devanshi-j@users.noreply.github.com>
2024-03-28 16:48:17 +0530

    Update app.js

--- complete/app.js
+++ complete/app.js
@@ -257,7 +257,7 @@ loadKnight(){
         });
         
         // ...inside the 'rotate' event listener
-        if (ev.initialise !== undefined) {
+       /* if (ev.initialise !== undefined) {
         self.startQuaternion = self.knight.object.quaternion.clone();
         } else {
         // Construct the rotation quaternion directly
@@ -270,7 +270,7 @@ loadKnight(){
         self.knight.object.quaternion.copy( self.startQuaternion );
         self.knight.object.rotateY( ev.theta );
         self.ui.updateElement('info', `rotate ${ev.theta.toFixed(3)}`
-        )};
+        )};*/
 
         this.renderer.setAnimationLoop(this.render.bind(this));
      
