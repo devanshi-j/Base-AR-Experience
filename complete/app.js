e1c8ace592c11c0a0a402e0f264a0d6631e0db96
devanshi-j <89416589+devanshi-j@users.noreply.github.com>
2024-04-15 22:20:45 +0530

    Update app.js

--- complete/app.js
+++ complete/app.js
@@ -16,7 +16,7 @@ class App {
 
         this.clock = new THREE.Clock();
 
-	this.loadingBar = new LoadingBar();
+	
 	
 	this.assetsPath = '../assets/';
 	
@@ -83,7 +83,7 @@ class App {
 
      loadKnight() {
 
-	
+	this.loadingBar = new LoadingBar();
         const loader = new GLTFLoader().setPath(this.assetsPath);
         const self = this;
 
